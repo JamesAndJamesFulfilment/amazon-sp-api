8431ae5a2861abb43753ab0866297c389ea9cd63
Michael <mikayel.margaryan@jamesandjames.com>
2022-03-02 15:50:49 +0400

    readmy file changed, testing endpoints added

--- lib/AmazonSPAPIEndpoint.php
+++ lib/AmazonSPAPIEndpoint.php
@@ -7,4 +7,8 @@ class AmazonSPAPIEndpoint
     public static $EUROPE = 'https://sellingpartnerapi-eu.amazon.com';
     public static $FAR_EAST = 'https://sellingpartnerapi-fe.amazon.com';
     public static $NORTH_AMERICA = 'https://sellingpartnerapi-na.amazon.com';
+
+    public static $EUROPE_TEST = 'https://sandbox.sellingpartnerapi-eu.amazon.com';
+    public static $FAR_EAST_TEST = 'https://sandbox.sellingpartnerapi-fe.amazon.com';
+    public static $NORTH_AMERICA_TEST = 'https://sandbox.sellingpartnerapi-na.amazon.com';
 }
